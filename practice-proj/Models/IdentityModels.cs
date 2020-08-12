6487c8e9435401c3f9fb7b7007f5a6f42e398a55
Stefan Seul <65729791+GeraltofPython@users.noreply.github.com>
2020-08-11 22:11:45 -0700

    adding register database

--- practice-proj/Models/IdentityModels.cs
+++ practice-proj/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace practice_proj.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("JPAPZAzure", throwIfV1Schema: false)
         {
         }
 
